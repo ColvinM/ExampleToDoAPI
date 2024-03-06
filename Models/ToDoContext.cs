e7e10081692c586456ba09d9bd3f2213cba24582
Maeghan Colvin <tazchicky2k@yahoo.com>
2024-03-05 21:57:17 -0500

    Add project files.

---
+++ Models/ToDoContext.cs
@@ -0,0 +1,14 @@
+﻿using ExampleToDoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExampleToDoApi.Models;
+
+public class ToDoContext : DbContext
+{
+    public ToDoContext(DbContextOptions<ToDoContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<ToDoItem> TodoItems { get; set; } = null!;
+}
